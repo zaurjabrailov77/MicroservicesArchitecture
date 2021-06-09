01e131cdd59e53371a3e3d77ea527fe39430099a
zaurjabrailov77 <66160217+zaurjabrailov77@users.noreply.github.com>
2021-06-09 10:58:25 +0400

    Add Cloud Config Server yml

---
+++ cloud-config-server/src/main/java/az/com/developerdba/cloudconfigserver/CloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package az.com.developerdba.cloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class CloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerApplication.class, args);
+		System.out.println("Started Cloud Config Server");
+	}
+}
